096ca97c901cb6eb28b30539a58aec71ae1aa758
mkrvi <mkrv.iryna@gmail.com>
2022-09-17 02:21:30 +0300

    final test

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,26 @@
+$(document).ready(function(){
+
+    $( function() {
+        $( "#accordion" ).accordion();
+    } );
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        center:true,
+        margin:15,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+});
 
 
